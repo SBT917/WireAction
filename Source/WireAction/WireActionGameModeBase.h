dc86c3ef636d8a8b4437321cd0f5a8193b1b9ca7
esuzero0917 <esuzero0917@gmail.com>
2023-08-19 16:12:32 +0900

    初期コミット

---
+++ Source/WireAction/WireActionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WireActionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIREACTION_API AWireActionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
